f13c780778b554768e9c03b87a595def301343dc
Aakash Sigdel <aakash.sigdel@gmail.com>
2014-12-26 15:30:04 +0545

    added styles

--- data/PushData.js
+++ data/PushData.js
@@ -1,15 +1,20 @@
 self.port.on("display", function(selectedString, noOfEntriesCounter) {
+	console.log('headphones    ' + noOfEntriesCounter);
 	var linkNode = document.createElement("A");
 	var linkNodeAttr = document.createAttribute("href");
-	linkNodeAttr.value = '#' + noOfEntriesCounter + 1;
+	linkNodeAttr.value = '#' + (noOfEntriesCounter + 1);
 	linkNode.setAttributeNode(linkNodeAttr);
 	var textnode = document.createTextNode(selectedString);
 	linkNode.appendChild(textnode);
 	
-	var node = document.createElement("LI");
-	node.appendChild(linkNode);
-	var firstChild = document.getElementById("content_list").firstChild;
-	document.getElementById("content_list").insertBefore(node, firstChild);
+	var tdNode = document.createElement("TD");
+	tdNode.appendChild(linkNode);
+
+	var trNode = document.createElement("TR");
+	trNode.appendChild(tdNode);
+
+	var firstChild = document.getElementById("content-table").firstChild;
+	document.getElementById("content-table").insertBefore(trNode, firstChild);
 });
 
 //Listen to link clicked on panel
